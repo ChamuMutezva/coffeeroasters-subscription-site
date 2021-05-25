8f57dd5ee0362d70ea33f8e13bc030fb29b70510
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-05-25 04:58:51 +0200

    fix: loaded images from datalist

---
+++ src/pages/components/dataList.js
@@ -0,0 +1,31 @@
+import gran from '../../assets/home/desktop/image-gran-espresso.png'
+import planalto from '../../assets/home/desktop/image-planalto.png'
+import piccolo from '../../assets/home/desktop/image-piccollo.png'
+import danche from '../../assets/home/desktop/image-danche.png'
+export const Collections = [
+    {
+        id: '1',
+      title: 'Gran Espresso',
+       summary: 'Light and flavorful blend with cocoa and black pepper for an intense experience',
+       imgUrl: gran,
+    },
+    {
+        id: '2',
+        title: 'Planalto',
+        summary: 'Brazilian dark roast with rich and velvety body, and hints of fruits and nuts',
+        imgUrl: planalto,
+    },
+    {
+        id: '3',
+        title: 'Piccollo',
+       summary: 'Mild and smooth blend featuring notes of toasted almond and dried cherry',   
+       imgUrl: piccolo,
+    },
+    {
+        id: '4',
+        title: 'Danche',
+        summary: 'Ethiopian hand-harvested blend densely packed with vibrant fruit notes',
+        imgUrl: danche,
+    }     
+      
+]
--- src/pages/home/mainHome.js
+++ src/pages/home/mainHome.js
@@ -1,8 +1,19 @@
+import { Collections } from '../components/dataList';
 const MainHome = () => {
-    return(
+    console.log(Collections)
+    return (
         <main className="main">
             <div className="collection">
                 <h2 className="main__title">Our collection</h2>
+                <ul>
+                    {Collections.map(item => {
+                        return <li key={item.id}>
+                                    <h3>{item.title}</h3>
+                                    <p>{item.summary}</p>
+                                    <img src={item.imgUrl} alt={`enjoy our ${item.title}`} />
+                                </li>
+                    })}
+                </ul>
             </div>
         </main>
     )
