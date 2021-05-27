cc2c80c84f89040c12cab93005a1d3cea48cfcd2
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-05-27 05:09:28 +0200

    update: styling tablet version - works section

--- src/pages/components/card.js
+++ src/pages/components/card.js
@@ -5,7 +5,9 @@ const Card = () => {
             {CardList.map(card => {
                 return <li className="card" key={card.id}>
                              <div className="card__wrapper">
-                                 <img className="card__img" src={card.imgUrl} alt={card.alt} />
+                                 <div className="card__img__wrapper">
+                                     <img className="card__img" src={card.imgUrl} alt={card.alt} />
+                                 </div>                                
                                  <div className="card__details">
                                      <h3 className="card__title">{card.title}</h3>
                                      <p className="card__summary">{card.summary}</p>
