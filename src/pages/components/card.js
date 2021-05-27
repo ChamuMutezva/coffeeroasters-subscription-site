16390691f1bc29bc247930fe840ccb2b46611bf0
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-05-27 22:14:18 +0200

    feat: desktop card styles

--- src/pages/components/card.js
+++ src/pages/components/card.js
@@ -5,9 +5,9 @@ const Card = () => {
             {CardList.map(card => {
                 return <li className="card" key={card.id}>
                              <div className="card__wrapper">
-                                 <div className="card__img__wrapper">
+                                 <div className="card__img__wrapper"> 
                                      <img className="card__img" src={card.imgUrl} alt={card.alt} />
-                                 </div>                                
+                                 </div>                             
                                  <div className="card__details">
                                      <h3 className="card__title">{card.title}</h3>
                                      <p className="card__summary">{card.summary}</p>
