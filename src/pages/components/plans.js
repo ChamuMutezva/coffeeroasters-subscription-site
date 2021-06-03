7b9c34d5fd43fd504cd736c512d219c2cfa8ae29
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-06-03 05:40:36 +0200

    feat: tablet and desktop display plan section

--- src/pages/components/plans.js
+++ src/pages/components/plans.js
@@ -10,21 +10,22 @@ const Plan = () => {
     }
 
     return (
-        <section>
+        <section className="plan__section" >
             <ul className="accordion">
                 {PlanSteps.map(plan => {
                     return <li key={plan.id}>
                         <div className="accordion__item">
                             <h3 className="accordion__header">
-                                <button aria-expanded="true" className={`accordion--btn ${show ? "collapsed" : ""}`} onClick={handleShow} data-toggle="collapse" data-target={`collapse${plan.id}`} >{plan.question}</button>
+                                <button aria-expanded="true" className={`accordion--btn ${show ? "collapsed" : ""}`} onClick={handleShow} data-toggle="collapse"
+                                 data-target={`collapse${plan.id}`} >{plan.question}</button>
                             </h3>
                             <div id={`collapse${plan.id}`} className={`plan__card ${show ? "collapse" : ""}`}>
                                 {plan.options.map(opt => {
                                     return <div className={`plan__select ${plan.name}`} key={opt.id}>
-                                        <h3 className={`wrapper__${plan.name}`}>
+                                        <h3 className={`plan__card__title wrapper__${plan.name}`}>
                                             {opt.type}
                                         </h3>
-                                        <p id={`${opt.sub}`}>{opt.answer}</p>
+                                        <p id={`${opt.sub}`} className={`plan__card__content ${opt.sub}`}>{opt.answer}</p>
                                     </div>
                                 })}
                             </div>
