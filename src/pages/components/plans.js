528a8afc91488ba8f1850893cd0bbe047157f936
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-06-03 21:11:05 +0200

    feat: custom radio buttons without the usual circle

--- src/pages/components/plans.js
+++ src/pages/components/plans.js
@@ -3,12 +3,17 @@ import { PlanSteps } from '../components/dataList'
 const Plan = () => {
     const [show, setShow] = useState(false)
     // const {name} = PlanSteps
-    // console.log(PlanSteps)
+    console.log(PlanSteps)
     const handleShow = () => {
         setShow(!show)
         console.log(show)
     }
-
+/*<div className={`plan__select ${plan.name}`} key={opt.id}>
+                                     <h3 className={`plan__card__title radio__big__text wrapper__${plan.name}`}>
+                                            {opt.type}
+                                        </h3>
+                                <p id={`${opt.sub}`} className={`plan__card__content radio__small__text ${opt.sub}`}>{opt.answer}</p>
+                                    </div> */
     return (
         <section className="plan__section" >
             <ul className="accordion">
@@ -17,15 +22,16 @@ const Plan = () => {
                         <div className="accordion__item">
                             <h3 className="accordion__header">
                                 <button aria-expanded="true" className={`accordion--btn ${show ? "collapsed" : ""}`} onClick={handleShow} data-toggle="collapse"
-                                 data-target={`collapse${plan.id}`} >{plan.question}</button>
+                                    data-target={`collapse${plan.id}`} >{plan.question}</button>
                             </h3>
                             <div id={`collapse${plan.id}`} className={`plan__card ${show ? "collapse" : ""}`}>
                                 {plan.options.map(opt => {
                                     return <div className={`plan__select ${plan.name}`} key={opt.id}>
-                                        <h3 className={`plan__card__title wrapper__${plan.name}`}>
-                                            {opt.type}
-                                        </h3>
-                                        <p id={`${opt.sub}`} className={`plan__card__content ${opt.sub}`}>{opt.answer}</p>
+                                        <input type="radio" name={plan.name} id={opt.sub} />
+                                        <label className="radio__label" htmlFor={opt.sub}>
+                                            <span className={`plan__card__title radio__big__text wrapper__${plan.name}`}>{opt.type}</span>
+                                            <span className={`plan__card__content radio__small__text ${opt.sub}`}>{opt.answer}</span>
+                                        </label>
                                     </div>
                                 })}
                             </div>
