cbedcc25e969ec3925cc06805b405ef0274cd229
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-06-06 22:42:13 +0200

    feat: subscribe modal - desktop version complete

--- src/pages/components/accordion.js
+++ src/pages/components/accordion.js
@@ -42,8 +42,18 @@ const Accordion = () => {
         if (typeof (radioData.preference === null)) {
             console.log("enter data")
         }
-
+       // document.body.classList.add("hide__scroll")
+        const modal = document.querySelector(".modal__subscribe")
+        modal.classList.toggle("subscribe__show")
         console.log(evt)
+        evt.preventDefault()
+    }
+
+    //handle subscribe form
+    const handleSubscribe = (evt) => {
+       // document.body.classList.remove("hide__scroll")
+        const modal = document.querySelector(".modal__subscribe")
+        modal.classList.toggle("subscribe__show")
         evt.preventDefault()
     }
     return (
@@ -60,7 +70,8 @@ const Accordion = () => {
                             <div id={`collapse${plan.id}`} className={`plan__card`}>
                                 {plan.options.map(opt => {
                                     return <div className={`plan__select ${plan.name}`} key={opt.id}>
-                                        <input type="radio" name={plan.name} id={opt.sub} onChange={onChange} />
+                                        <input type="radio" name={plan.name} id={opt.sub}
+                                         onChange={onChange} />
                                         <label className="radio__label" htmlFor={opt.sub}>
                                             <span className={`plan__card__title radio__big__text wrapper__${plan.name}`}>{opt.type}</span>
                                             <span className={`plan__card__content radio__small__text ${opt.sub}`}>{opt.answer}</span>
@@ -91,9 +102,11 @@ const Accordion = () => {
             </div>
 
             <div className="modal__subscribe">
-                <div className="order__summary">
-                    <h3 className="order__title">Order Summary</h3>
-                    <p className="order__content">
+                <form className="subscribe__summary" onSubmit={handleSubscribe}>
+                    <div className="subscribe__title__wrapper">
+                        <h3 className="subscribe__title">Order Summary</h3>
+                    </div>
+                    <p className="subscribe__content">
                         “I drink my coffee as
                              <span className="ordered__item"> {radioData.preference} </span>, with a
                              <span className="ordered__item"> {radioData.bean} </span> type of bean.
@@ -101,12 +114,12 @@ const Accordion = () => {
                              <span className="ordered__item"> {radioData.grind} </span>, sent to me
                              <span className="ordered__item"> {deliver}</span>.”
                     </p>
-                    <p>
+                    <p className="subscribe__confirm">
                         Is this correct? You can proceed to checkout or go back to plan selection if something
                         is off. Subscription discount codes can also be redeemed at the checkout.
                      </p>
-                     <button type="submit" className="hero--btn order--btn">Create your plan</button>
-                </div>
+                     <button type="submit" className="hero--btn order--btn checkout--btn">Checkout $14 / mo</button>
+                </form>
   
             </div>
         </div>
